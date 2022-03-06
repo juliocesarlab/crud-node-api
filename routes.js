c06d3635254f24edec59a00bcd17bac7c60b58c0
julin julin <juliostdfac@gmail.com>
2022-03-06 10:32:34 -0300

    not found

--- routes.js
+++ routes.js
@@ -3,7 +3,7 @@ import Clients from './controllers/clientsController.js'
 
 export const clientsRouter = Router();
 
-clientsRouter.get('/', (req, res) => res.send("hey"))
+clientsRouter.get('/', (req, res) => res.status(404).send("Not found"))
 clientsRouter.get('/clients', Clients.getAll)
 clientsRouter.get('/clients/disable', Clients.getAllDisable)
 clientsRouter.post('/client/create', Clients.create)
