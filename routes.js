b1eb2bbac41699459a7b6add0c8bc177ef4281d9
julin julin <juliostdfac@gmail.com>
2022-03-05 23:55:30 -0300

    heroku test

--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@ import Clients from './controllers/clientsController.js'
 
 export const clientsRouter = Router();
 
+clientsRouter.get('/', (req, res) => res.send("hey"))
 clientsRouter.get('/clients', Clients.getAll)
 clientsRouter.get('/clients/disable', Clients.getAllDisable)
 clientsRouter.post('/client/create', Clients.create)
